93fcd07095e28a458173beb1781585d522e7f02f
Aman Sharma <amans7990@gmail.com>
2020-11-27 19:37:09 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
-import React from "react";
-import "./App.css";
-import Button from "./Component/Button";
+import React, { useState } from "react";
 import Display from "./Component/Display";
-function App() {
+import Button from "./Component/Button";
+import "./App.css";
+const App = () => {
+  const [time, setTime] = useState({ millisec: 0, sec: 0, min: 0, hrs: 0 });
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  let updatedMs = time.millisec,
+    updatedS = time.sec,
+    updatedM = time.min,
+    updatedH = time.hrs;
+
+  const run = () => {
+    if (updatedM === 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ millisec: updatedMs, sec: updatedS, min: updatedM, hrs: updatedH });
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ millisec: 0, sec: 0, min: 0, hrs: 0 });
+  };
+
+  const resume = () => start();
+
   return (
     <div className="container">
       <div className="clock-holder">
         <div className="stopwatch">
-          <Button />
-          <Display />
+          <Display time={time} />
+          <Button
+            status={status}
+            resume={resume}
+            reset={reset}
+            stop={stop}
+            start={start}
+          />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Component/Display.js
+++ src/Component/Display.js
@@ -1,10 +1,33 @@
 import React from "react";
-const Button = () => {
+const DisplayComponent = (props) => {
+  const h = () => {
+    if (props.time.hrs === 0) {
+      return "";
+    } else {
+      return (
+        <span>
+          {props.time.hrs >= 10 ? props.time.hrs : "0" + props.time.hrs}
+        </span>
+      );
+    }
+  };
   return (
-    <div className="container" style={{ color: "black" }}>
-      HI
+    <div>
+      {h()}&nbsp;
+      <span>
+        {props.time.min >= 10 ? props.time.min : "0" + props.time.min}
+      </span>
+      &nbsp;:&nbsp;
+      <span>
+        {props.time.sec >= 10 ? props.time.sec : "0" + props.time.sec}
+      </span>
+      &nbsp;:&nbsp;
+      <span>
+        {props.time.millisec >= 10
+          ? props.time.millisec
+          : "0" + props.time.millisec}
+      </span>
     </div>
   );
 };
-
-export default Button;
+export default DisplayComponent;
