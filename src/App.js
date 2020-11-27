1f4ea3b6737cd3e8ff2953869d3a48a940688690
Aman Sharma <amans7990@gmail.com>
2020-11-27 16:45:35 +0530

    Up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-
+import Button from "./Component/Button";
+import Display from "./Component/Display";
 function App() {
-
   return (
     <div className="container">
-     <div className="clock-holder">
-       <div className="stopwatch">
-
-
-       </div>
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <Button />
+          <Display />
+        </div>
       </div>
     </div>
   );
---
+++ src/Component/Display.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Button = () => {
+  return (
+    <div className="container" style={{ color: "black" }}>
+      HI
+    </div>
+  );
+};
+
+export default Button;
